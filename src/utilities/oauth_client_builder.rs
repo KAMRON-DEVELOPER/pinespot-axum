6fcfb811de0a6b6761cf8cbba435976c7189e9d5
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-09-30 10:46:27 +0500

    google, github oaut implemented

--- src/utilities/oauth_client_builder.rs
+++ src/utilities/oauth_client_builder.rs
@@ -2,7 +2,6 @@ use oauth2::{
     AuthUrl, ClientId, ClientSecret, RedirectUrl, RevocationUrl, TokenUrl, basic::BasicClient,
 };
 use serde::Deserialize;
-use tracing::debug;
 
 use crate::utilities::{config::Config, errors::AppError};
 
